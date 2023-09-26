4790deb2fbe65279f6258ee52ac0df1454f94298
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-09-26 16:04:27 +0300

    Completed project

--- website/__init__.py
+++ website/__init__.py
@@ -4,36 +4,38 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = 'database.db'
+DB_NAME = "database.db"
+
+
 def create_app():
-  app = Flask(__name__) #Reps name of file. This is for initialization
-  app.config['SECRET_KEY']= 'yukiosrandomshit'
-  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
-  db.init_app(app)
+    from .views import views
+    from .auth import auth
 
-  from .views import views
-  from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-  app.register_blueprint(views, url_prefix='/')
-  app.register_blueprint(auth, url_prefix='/')
-
-  from .models import User, Note
-  with app.app_context():
+    from .models import User, Note
+    
+    with app.app_context():
         db.create_all()
 
-  login_manager = LoginManager()
-  login_manager.login_view = 'auth.login'
-  login_manager.init_app(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
-  @login_manager.user_loader
-  def load_user(id):
-      return User.query.get(int(id))
-  
-  return app
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
 
 def create_database(app):
-  if not path.exists(DB_NAME):
-        with app.app_context():
-            db.create_all()
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
         print('Created Database!')
--- website/models.py
+++ website/models.py
@@ -2,20 +2,17 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class User(db.Model, UserMixin):
-  # __tablename__='users'
-  id = db.Column(db.Integer, primary_key=True)
-  email = db.Column(db.String(150), unique=True)
-  password = db.Column(db.String(50))
-  first_name = db.Column(db.String)
-  notes = db.relationship('Note')
-  # notes = db.relationship('Note', backref='user')
 
 class Note(db.Model):
-  # __tablename__ ='notes'
-  id = db.Column(db.Integer, primary_key=True)
-  data = db.Column(db.String)
-  date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  # user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
