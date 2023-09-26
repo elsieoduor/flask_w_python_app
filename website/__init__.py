8804ef8ad58764bcf42feb5428ebfce107f5b0f3
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-09-26 13:23:29 +0300

    Initialized database

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__) #Reps name of file. This is for initialization
   app.config['SECRET_KEY']= 'yukiosrandomshit'
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+  db.init_app(app)
 
   from .views import views
   from .auth import auth
